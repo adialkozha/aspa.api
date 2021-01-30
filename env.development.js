0a58e6f8b608e6fe68951ab29a8b0b2dd567b07d
adialkozha <49197733+adialkozha@users.noreply.github.com>
2021-01-30 16:55:04 +0600

    upd:add users service

---
+++ env.development.js
@@ -0,0 +1,8 @@
+module.exports = {
+    NODE_ENV: 'development',
+    TRANSPORTER: 'nats://localhost:4222',
+    HOT_RELOAD: true,
+    TRACE: true,
+    LOG_LEVEL: 'info',
+    LOG_FORMATTER: 'full'
+}
