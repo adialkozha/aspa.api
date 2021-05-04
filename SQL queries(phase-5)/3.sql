86f295d1cedf6629350e934a7b89971b816585ff
Aruzhan <44058615+aruzhanabay@users.noreply.github.com>
2021-05-04 22:32:14 +0600

    Update 3.sql

--- SQL queries(phase-5)/3.sql
+++ SQL queries(phase-5)/3.sql
@@ -1,8 +1,6 @@
 -- SQLite
---3 What if the product is popular enough and there are too many search results? 
-select product.product_name as 'product', ProductCategory.Category_id 'specified category'
-FROM Product 
-JOIN ProductCategory on Product.Uniq_Id = ProductCategory.Product_id 
-JOIN Category ON ProductCategory.Category_id= Category.Category_id
-where Category.Title = 'any specified subcategory' 
-GROUP BY product.product_name, ProductCategory.Category_id ORDER BY ProductCategory.Category_id;
+--3 Output the address and basketId of Umer Pathan from the table
+select orders.Address, Orders.BasketId from users 
+JOIN ORDERs ON users.Id= orders.User
+JOIN Baskets ON Baskets.basketId = orders.basketId
+WHERE Users.Name = 'UMER PATHAN';
