aef0ea27809cf145e07fd3a4120b611ba0fdb7fa
Aruzhan <44058615+aruzhanabay@users.noreply.github.com>
2021-05-04 22:28:39 +0600

    Update 2.sql

--- SQL queries(phase-5)/2.sql
+++ SQL queries(phase-5)/2.sql
@@ -1,5 +1,8 @@
 -- SQLite
 
---2  Can we filter the goods by the rating of the product?
-Select Product.product_name as 'popular product', ProductReview.Rating 
-from Product, ProductReview WHERE Product.Uniq_Id = ProductReview.Product_id GROUP BY Product.product_name, ProductReview.Rating ORDER BY ProductReview.Rating; 
+--2  Identify user by address
+SELECT UserId
+FROM Products 
+JOIN Baskets ON products.productId=baskets.productId
+JOIN orders ON baskets.basketId=orders.basketId
+WHERE Orders.Address = '4307 E Chadwick CT'; --2 tuples
